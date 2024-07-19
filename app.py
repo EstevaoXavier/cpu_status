619e86eec422f55f4807672ce743acedd8d4b3bf
EstevaoXavier <xestevao0@gmail.com>
2024-07-19 17:45:22 -0300

    criando a métrica

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-print('Olá mundo')
+#para corrigir o problema do ttk.meter
+from PIL import Image
+Image.CUBIC = Image.BICUBIC
+#importações
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+
+#definição do app
+app = ttk.Window(themename="cosmo")
+app.title("CPU STATUS")
+app.geometry("800x400")
+
+meter = ttk.Meter(
+    metersize=80,
+    padding=30,
+    amountused=0,
+    amounttotal= f"2000",
+    metertype="semi",
+    subtext="miles per hour",
+    subtextfont= f"-size 6",
+    textfont="-size 11 -weight bold",
+    interactive=True,
+)
+meter.pack()
+
+meter.configure(amountused = 70)
+
+meter.configure(subtext="loading...")
+
+app.mainloop()
