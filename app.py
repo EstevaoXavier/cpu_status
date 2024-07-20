d02bbdc2c3989a6222589b626b873dc9d8475220
EstevaoXavier <xestevao0@gmail.com>
2024-07-20 20:05:28 -0300

    terminando a os meter e começando as outras informações

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ Image.CUBIC = Image.BICUBIC
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import psutil
+import time
 
 def verificar_temperatura():
     try:
@@ -23,23 +24,35 @@ def verificar_temperatura():
     except AttributeError:
         # Caso a funcionalidade não seja suportada pelo sistema
         print("A funcionalidade de sensores de temperatura não é suportada no seu sistema.")
+def fazer_analise():
+        info_disco = psutil.disk_usage('/')
+        info_ram = psutil.virtual_memory()
+
+        meter_memory.configure(amountused = f'{info_disco.used / (1024**3):.2f}')
+        meter_ram.configure(amountused = f'{info_ram.used / (1024**3):.2f}')
+        print('entou na funcao')
+
+
+
 
 #definição do app
 app = ttk.Window(themename="cosmo")
 app.title("CPU STATUS")
 app.geometry("400x700")
 frame = ttk.Frame(app)
-frame.grid(row=0, column=0, padx=10, pady=10, sticky="n")
+frame.grid(row=0, column=0, padx=66, pady=10)
+app.resizable(width=False, height=False)
 
+button_att = ttk.Button(app, text="Fazer analise", command=fazer_analise)
 
+#  Métrica do Disco
 info_disco = psutil.disk_usage('/')
-
 label_memoria = ttk.Label(app, text='Disco e RAM')
 
 meter_memory = ttk.Meter(
     frame,
     metersize=120,
-    padding=20,
+    padding=0,
     amountused=0,
     amounttotal= f"{info_disco.total / (1024**3):.2f}",
     metertype="full",
@@ -69,18 +82,21 @@ meter_ram = ttk.Meter(
     bootstyle="info",
 )
 
-label_memoria.grid(row=0, column=0, padx=5, pady=5, sticky='n')
+# Janela
 
-meter_memory.grid(row=1, column=0, padx=5, pady=5, sticky='n')
-meter_memory.configure(amountused = f'{info_disco.used / (1024**3):.2f}')
+label_memoria.grid(row=0, column=0, padx=0, pady=0, sticky='n')
 
-meter_ram.grid(row=1, column=1, padx=5, pady=5, sticky='n')
-meter_ram.configure(amountused = f'{info_ram.used / (1024**3):.2f}')
+meter_memory.grid(row=1, column=0, padx=0, pady=10, sticky='w')
+meter_ram.grid(row=1, column=1, padx=0, pady=10, sticky='e')
 
-# Separador
-separator = ttk.Separator(app, orient='horizontal',)
-separator.grid(row=3, column=0, padx=5, pady=5, sticky='n',)
+separador = ttk.Separator(app, orient='horizontal',)
+separador.grid(row=2, column=0, padx=0, pady=10, sticky='ew',)
+
+button_att.grid(row=3, column=0, padx=0, pady=10, sticky="ew")
 
 
+frame.grid_rowconfigure(0, weight=1)
+frame.grid_columnconfigure(0, weight=1)
+frame.grid_columnconfigure(1, weight=1)
 
 app.mainloop()
